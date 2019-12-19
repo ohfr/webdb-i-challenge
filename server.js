a7f2d194460f0764f933af7e923fceaf6edbdee2
Daniel Martin <danielmartin1205@gmail.com>
2019-12-18 19:03:06 -0500

    added MVP and stretch SQL queries

--- server.js
+++ server.js
@@ -6,4 +6,66 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", async (req,res, next) => {
+    try {
+        res.json(await db.select("*").from("accounts"));
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.get("/:id", async (req,res, next) => {
+    try {
+        res.json(await db("accounts").where("id", req.params.id).first());
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        };
+
+        const [id] = await db("accounts").insert(payload);
+
+        res.json(await db("accounts").where("id", id).first());
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.put("/:id", async (req,res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        };
+        
+        await db("accounts").where("id", req.params.id).update(payload);
+
+        res.json(await db("accounts").where("id", req.params.id).first());
+        
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.delete("/:id", async (req, res, next) => {
+    try {
+        res.json(await db("accounts").where("id", req.params.id).del());
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({message: "Something went wrong"})
+});
+
 module.exports = server;
